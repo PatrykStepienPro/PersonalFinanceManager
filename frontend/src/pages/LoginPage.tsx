4f0c8e8305e7d20e8c40cd234d8c87e7ecdaf5dc
PatrykStepienPro <patryk.stepien.pro@gmail.com>
2026-07-07 16:43:05 +0200

    Intall UI package - shadcn, styling login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAuth } from "../context/AuthContext";
 import { login } from "../api/auth.api";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const loginSchma = z.object({
     email: z.email("Nieprawidłowy adress email"),
@@ -19,21 +23,43 @@ function LoginPage() {
         resolver: zodResolver(loginSchma)
     });
 
-  const onSubmit = async (data: LoginFormData) => {
-      const { accessToken } = await login(data)
-      auth.login(accessToken)
-      navigate('/')
-  }
+    const onSubmit = async (data: LoginFormData) => {
+        const { accessToken } = await login(data)
+        auth.login(accessToken)
+        navigate('/')
+    }
 
-    return <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email")} placeholder="Email" />
-        {errors.email && <p>{errors.email.message}</p>}
+    return <div className="min-h-screen flex items-center justify-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm">
+            <Card className="w-full max-w-sm">
+                <CardHeader className="justify-center">
+                    <CardTitle>
+                        Logowanie
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
 
-        <input {...register("password")} type="password" placeholder="Hasło" />
-        {errors.password && <p>{errors.password.message}</p>}
+                    <Field>
+                        <FieldLabel>Email</FieldLabel>
+                        <Input {...register("email")} placeholder="Email" />
+                        {errors.email && <FieldDescription style={{ color: "red" }}>{errors.email.message}</FieldDescription>}
+                    </Field>
 
-        <button type="submit">Zaloguj</button>
-    </form>
+                    <Field>
+                        <FieldLabel>Hasło</FieldLabel>
+                        <Input {...register("password")} type="password" placeholder="Hasło" />
+                        {errors.password && <FieldDescription style={{ color: "red" }}>{errors.password.message}</FieldDescription>}
+                    </Field>
+
+                </CardContent>
+                <CardAction className="flex-col gap-2 w-full p-4">
+                    <Button type="submit" className="w-full">
+                        Zaloguj
+                    </Button>
+                </CardAction>
+            </Card>
+        </form>
+    </div >
 }
 
 export default LoginPage
